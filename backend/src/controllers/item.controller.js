24d1929d1fcfc00cf0ae58f122639dd6627de93b
VaibhavAgarwal452 <vaibhavagarwal78@gmail.com>
2024-04-30 22:41:15 +0530

    made the item route and controller

--- backend/src/controllers/item.controller.js
+++ backend/src/controllers/item.controller.js
@@ -4,7 +4,67 @@ import { Item } from "../models/item.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createItem = asyncHandler(async (req, res) => {
+    const { title, description, category, condition, location } = req.body
 
+    if (!title || !description) {
+        throw new ApiError(400, "Title and description is required")
+    }
+
+    const item = await Item.create({
+        title,
+        description,
+        category,
+        condition,
+        location,
+        owner: req.user._id
+    })
+
+    if (!item) {
+        throw new ApiError(500, "Item cannot be created")
+    }
+    return res.status(201).json(new ApiResponse(200, item, "Item created successfully"))
 })
 
-export { createItem }
+const updateItem = asyncHandler(async (req, res) => {
+    const { itemId } = req.params
+    const { title, description } = req.body
+    const item = await Item.findByIdAndUpdate(itemId, {
+        $set: {
+            title,
+            description
+        }
+    },
+        {
+            new: true
+        })
+
+    if (!item) {
+        throw new ApiError(500, "Api Cannot be updated")
+    }
+
+    return res.status(202).json(new ApiResponse(200, item, "Item is successfully updated"))
+})
+
+const deleteItem = asyncHandler(async (req, res) => {
+    const { itemId } = req.params
+
+    const item = await Item.findByIdAndDelete(itemId)
+    if (!item) {
+        throw new ApiError(500, "Item cannot be deleted, please send correct itemID")
+    }
+
+    return res.status(200).json(new ApiResponse(200, item, "Item deleted successfully"))
+})
+
+const getItem = asyncHandler(async (req, res) => {
+    const { itemId } = req.params
+    const item = await Item.findById(itemId)
+    if (!item) {
+        throw new ApiError(500, "Please enter correct Item id")
+    }
+
+    return res.status(200).json(new ApiResponse(200, item, "Item fetched successfully"))
+})
+
+
+export { createItem, deleteItem, updateItem, getItem }
--- backend/src/routes/item.routes.js
+++ backend/src/routes/item.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createItem } from "../controllers/item.controller.js";
+import { createItem, updateItem, deleteItem, getItem } from "../controllers/item.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+router.use(verifyJWT)
 router.route("/create").post(createItem)
+router.route("/:itemId").patch(updateItem).delete(deleteItem).get(getItem)
+
 
 export default router;
 
